285c47612d07662529041cbe4eedd6be8296789a
Deepak Guptha <vzan2012@gmail.com>
2021-01-31 21:45:05 +0530

    feat: Updation of Directives Assignment - three

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,30 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styles: [`h3{
     color: blue;
-}`, `p {color: red;}`]
+}`, `p {color: red;}`, `.log-Fifth {
+  background: blue;
+  color: white;
+}`]
   // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   titleName = 'Welcome la-app';
 
   userName: string = '';
+  showDetails: boolean = false;
+  numClicks: number = 0;
+  clickArray: string[] = []
 
   onReset() {
     this.userName = ''
   }
+
+  onShowDetails() {
+    this.numClicks = this.numClicks + 1;
+    this.showDetails = !this.showDetails;
+    let clickDetails = `Number of clicks: ${this.numClicks} - ${new Date()}`
+    this.clickArray.push(clickDetails);
+  }
+
+
 }
